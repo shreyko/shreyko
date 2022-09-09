6b8e164ff4a6bf039de55ed8a8a8a3a3debe162e
Shrey Kohli <skohli.ddn@icloud.com>
2022-09-09 19:52:04 -0400

    deployer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+app.use(express.static(__dirname));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
