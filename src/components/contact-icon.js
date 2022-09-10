ecd254a9658dc3d8d81b6ce3742b325b6558857c
Shrey Kohli <skohli.ddn@icloud.com>
2022-09-10 01:43:04 -0400

    debug

--- src/components/contact-icon.js
+++ src/components/contact-icon.js
@@ -6,8 +6,8 @@ export default function Contact_icon()
     return(
         <div>
             <div id='contact'>
-                <div id="linkedin"><a href="https://www.linkedin.com/in/shrey-kohli-537a7921b/" ><img src={require('./linkedin.png')} alt = "LINKEDIN" id = "linkimg"></img></a></div>
-                <div id = "github"><a href = "https://github.com/shreyko"><img src={require('./github.png')} alt = "GITHUB" id = "gitimg"></img></a></div>
+                <div id="linkedin"><a href="https://www.linkedin.com/in/shrey-kohli-537a7921b/" target="_blank" ><img src={require('./linkedin.png')} alt = "LINKEDIN" id = "linkimg"></img></a></div>
+                <div id = "github"><a href = "https://github.com/shreyko" target="_blank"><img src={require('./github.png')} alt = "GITHUB" id = "gitimg"></img></a></div>
             </div>
         </div>
 
