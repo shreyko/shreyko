9ce6211ca05731c6ac03ca2a73d5909de89e162c
Shrey Kohli <skohli.ddn@icloud.com>
2022-09-10 23:49:22 -0400

    resume

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
                     <div className="link_about"><a href = "#about_a" >About</a></div>
                     <div className="link_exp"><a href = "#exper" >Experience</a></div>
                     <div className="link_contact"><a href = "#contacts" >Contact Me</a></div>
-                    <div className = "link_resume"><a href = "https://s3.amazonaws.com/handshake.production/documents/documents/029/807/886/original/Resume_shrey_kohli_final.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2HSNSGACXF6KKT2H%2F20220910%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220910T041816Z&X-Amz-Expires=10&X-Amz-SignedHeaders=host&X-Amz-Signature=d6a04c6eca81bb59ab885577ab777c316c3f06fc5e49f5c5d2bf41f730c2c43e" target = "_blank" download>Resume</a></div>
+                    <div className = "link_resume"><a href = "https://www.linkedin.com/feed/update/urn:li:activity:6974574027247898624/" target = "_blank" download>Resume</a></div>
                 </div>
         <div className = 'dropdown' >
             <a href="#" onClick= {() => setisOpen(!isOpen)}>
@@ -25,7 +25,7 @@ export default function Navbar() {
             <div className="link_abouth"><a href = "#about_a" >About</a></div>
             <div className="link_exph"><a href = "#exper" >Experience</a></div>
             <div className="link_contacth"><a href = "#contacts" >Contact Me</a></div>
-            <div className = "link_resume"><a href = "https://s3.amazonaws.com/handshake.production/documents/documents/029/807/886/original/Resume_shrey_kohli_final.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2HSNSGACXF6KKT2H%2F20220910%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220910T041816Z&X-Amz-Expires=10&X-Amz-SignedHeaders=host&X-Amz-Signature=d6a04c6eca81bb59ab885577ab777c316c3f06fc5e49f5c5d2bf41f730c2c43e" target = "_blank" download>Resume</a></div>
+            <div className = "link_resume"><a href = "https://www.linkedin.com/feed/update/urn:li:activity:6974574027247898624/" target = "_blank" download>Resume</a></div>
         </div>
         </div>
 
